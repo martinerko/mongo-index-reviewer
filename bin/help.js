f127ed3cc8174e605da4fce69e37d70ad56897ee
martinerko <martinerko@gmail.com>
2016-05-14 21:20:09 +1000

    CLI version.

---
+++ bin/help.js
@@ -0,0 +1,18 @@
+/*!
+ * mongo-index-reviewer
+ * Copyright(c) 2016 martinerko <martinerko@gmail.com>
+ * MIT Licensed
+ */
+
+module.exports = function help() {
+  console.log('');
+  console.log('  Usage: program connectionString [options]\n');
+  console.log('  Options:\n');
+  console.log('    --username <username>    specifies a username with which to authenticate to a MongoDB database that uses authentication.');
+  console.log('    --password <password>    specifies a password with which to authenticate to a MongoDB database that uses authentication.');
+  console.log('    --indexFile <filename>   specifies the file (source of truth) with createIndex statements (required).');
+  console.log('');
+  console.log('  Sample:\n')
+  console.log('    ./bin/index localhost:27017/test --indexFile ./reviewedIndexes.js')
+  console.log('    ./bin/index ds012345.myserver.com:56789/dbname --username dbuser --password dbpassword --indexFile ./reviewedIndexes.js')
+};
