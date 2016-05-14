e02a27dfdb0e74eb9bfe25ee7b5db3dae0f3595c
martinerko <martinerko@gmail.com>
2016-05-14 19:38:17 +1000

    Database processor collecting info about indexes.

---
+++ sample/dbProcessor.js
@@ -0,0 +1,22 @@
+/*!
+ * mongo-index-reviewer
+ * Copyright(c) 2016 martinerko <martinerko@gmail.com>
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var dbProcessor = require("../lib/dbProcessor");
+var connectionString = "localhost:27017/library";
+
+dbProcessor({
+  connectionString: connectionString
+}, function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
