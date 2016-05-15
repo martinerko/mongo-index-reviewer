ce31b526a64d71da8aed71f50f71c7994c2c611c
martinerko <martinerko@gmail.com>
2016-05-15 21:33:06 +1000

    Version 1.0.0

--- bin/help.js
+++ bin/help.js
@@ -6,13 +6,13 @@
 
 module.exports = function help() {
   console.log('');
-  console.log('  Usage: program connectionString [options]\n');
+  console.log('  Usage: mongo-index-reviewer connectionString [options]\n');
   console.log('  Options:\n');
   console.log('    --username <username>    specifies a username with which to authenticate to a MongoDB database that uses authentication.');
   console.log('    --password <password>    specifies a password with which to authenticate to a MongoDB database that uses authentication.');
   console.log('    --indexFile <filename>   specifies the file (source of truth) with createIndex statements (required).');
   console.log('');
   console.log('  Sample:\n')
-  console.log('    ./bin/index localhost:27017/test --indexFile ./reviewedIndexes.js')
-  console.log('    ./bin/index ds012345.myserver.com:56789/dbname --username dbuser --password dbpassword --indexFile ./reviewedIndexes.js')
+  console.log('    mongo-index-reviewer localhost:27017/test --indexFile ./reviewedIndexes.js')
+  console.log('    mongo-index-reviewer ds012345.myserver.com:56789/dbname --username dbuser --password dbpassword --indexFile ./reviewedIndexes.js')
 };
